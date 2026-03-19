432b195afc1c3522dbce9b9114872945a6d395d8
Donatus Dee <getdzidon@gmail.com>
2026-03-19 15:44:47 +0100

    test infra deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,10 +37,14 @@ data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "this" {
   name = module.eks.cluster_name
+
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
+
+  depends_on = [module.eks]
 }
 
 data "tls_certificate" "eks_oidc" {
