5b609302fb6b9d5230b6c70bc12f3c51574db331
Donatus Dee <getdzidon@gmail.com>
2026-03-19 17:09:42 +0100

    test infra deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ data "aws_iam_openid_connect_provider" "github" {
 # ── Providers ────────────────────────────────────────────────────────────────
 
 provider "helm" {
-  kubernetes {
+  kubernetes ={
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
